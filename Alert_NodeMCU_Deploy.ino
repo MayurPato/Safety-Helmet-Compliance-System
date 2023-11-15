7ab738c45542798e9ae4f0efb9ce4e918cc37931
Mayur Patowary <88105088+MayurPato@users.noreply.github.com>
2023-11-15 16:43:57 +0530

    Add files via upload

---
+++ Alert_NodeMCU_Deploy.ino
@@ -0,0 +1,48 @@
+#define BLYNK_TEMPLATE_ID "TMPL39zmk2BMR"
+#define BLYNK_DEVICE_NAME "Tarp Project"
+#define BLYNK_AUTH_TOKEN "Ge1vZ1bLCsqGz2JchLLDjjFLpqAqpawt"
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+int alert;
+
+char auth[] = BLYNK_AUTH_TOKEN ;
+char ssid[] = "";
+char pass[] = "";
+
+BLYNK_WRITE(V0)
+{
+  alert = param.asInt();
+  if (alert == 1){
+    Serial.println("NO HELMET DETECTED!");
+  }
+  
+}
+
+void helmet()
+{
+  if (alert == 1){
+    digitalWrite(D1, HIGH);
+    delay(500);
+  }
+  else{
+    digitalWrite(D1, LOW);
+    delay(200);
+  }
+}
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  pinMode(D1, OUTPUT);
+}
+
+void loop()
+{
+  Blynk.run();
+  helmet();
+}
